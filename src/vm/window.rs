611bebf577f7baaf60492e39367bbe689de809c0
Feydral <lian.hable.feathertech@gmail.com>
2026-02-24 14:54:00 +0100

    Some clean-up, merge Window and Canvas, remove unnecessary methodes

--- src/vm/window.rs
+++ src/vm/window.rs
@@ -8,14 +8,14 @@ pub struct Window {
 }
 
 impl Window {
-    pub const TITLE: &str = "Canvas";
-    pub const WINDOW_WIDTH: usize = 768;
-    pub const WINDOW_HEIGHT: usize = 768;
+    pub const TITLE: &str = "Xis Virtual Machine";
+    pub const WINDOW_WIDTH: u32 = 768;
+    pub const WINDOW_HEIGHT: u32 = 768;
 
-    pub const BUFFER_WIDTH: usize = Self::WINDOW_WIDTH / 3;
-    pub const BUFFER_HEIGHT: usize = Self::WINDOW_HEIGHT / 3;
+    pub const BUFFER_WIDTH: u32 = Self::WINDOW_WIDTH / 3;
+    pub const BUFFER_HEIGHT: u32 = Self::WINDOW_HEIGHT / 3;
 
-    const TARGET_FPS: usize = 64;
+    const TARGET_FPS: u32 = 64;
 
     const WINDOW_OPTIONS: WindowOptions = WindowOptions {
         scale: minifb::Scale::X1,
@@ -31,33 +31,42 @@ impl Window {
     pub fn new() -> Self {
         let mut native_window = minifb::Window::new(
             &Self::TITLE,
-            Self::WINDOW_WIDTH,
-            Self::WINDOW_HEIGHT,
+            Self::WINDOW_WIDTH as usize,
+            Self::WINDOW_HEIGHT as usize,
             Self::WINDOW_OPTIONS
         ).unwrap();
 
-        native_window.set_target_fps(Self::TARGET_FPS);
+        native_window.set_target_fps(Self::TARGET_FPS as usize);
 
         Self {
-            buffer: vec![0u32; Self::BUFFER_WIDTH * Self::BUFFER_HEIGHT],
+            buffer: vec![0u32; (Self::BUFFER_WIDTH * Self::BUFFER_HEIGHT) as usize],
             native_window,
         }
     }
 
-    pub fn update_buffer(&mut self, buffer: &[&[Float3]]) {
-        for x in 0..Self::BUFFER_WIDTH {
-            for y in 0..Self::BUFFER_HEIGHT {
-                let idx = y * Self::BUFFER_WIDTH + x;
-                self.buffer[idx] = mathf::float3_to_u32_rgb(buffer[x][y]);
-            }
-        }
-
-        if let Err(e) = self.native_window.update_with_buffer(&self.buffer, Self::BUFFER_WIDTH, Self::BUFFER_HEIGHT) {
+    pub fn update_buffer(&mut self) {
+        if let Err(e) = self.native_window.update_with_buffer(&self.buffer, Self::BUFFER_WIDTH as usize, Self::BUFFER_HEIGHT as usize) {
             eprintln!("Error updating buffer: {}", e);
         }
     }
 
     pub fn is_open(&self) -> bool {
         self.native_window.is_open()
+    }
+
+    pub fn set_pixel(&mut self, x: u32, y: u32, color: Float3) {
+        if x < Window::BUFFER_WIDTH && y < Window::BUFFER_HEIGHT {
+            let index = y as usize * Window::BUFFER_WIDTH as usize + x as usize;
+            self.buffer[index] = mathf::float3_to_u32_rgb(color);
+        }
+    }
+
+    pub fn get_pixel(&self, x: u32, y: u32) -> Float3 {
+        if x < Self::BUFFER_WIDTH && y < Self::BUFFER_HEIGHT {
+            let index = y as usize * Self::BUFFER_WIDTH as usize + x as usize;
+            mathf::u32_to_float3_rgb(self.buffer[index])
+        } else {
+            Float3::ZERO
+        }
     }
 }
