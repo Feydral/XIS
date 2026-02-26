916d88f265ab94c9be6cbe8a614b3088010d6fba
Feydral <lian.hable.feathertech@gmail.com>
2026-02-26 12:18:33 +0100

    Make small docs in mathi

--- src/math/mathi.rs
+++ src/math/mathi.rs
@@ -2,6 +2,7 @@
 
 use crate::math::numerics::uint2::UInt2;
 
+#[doc = "Returns the xy coordinate of a specific index in an imaginary 2D canvas."]
 #[inline]
 pub fn index_to_xy(index: u32, width: u32, height: u32) -> UInt2 {
     if index > width * height {
@@ -12,6 +13,7 @@ pub fn index_to_xy(index: u32, width: u32, height: u32) -> UInt2 {
     UInt2::new(x, y)
 }
 
+#[doc = "Returns the index of a specific xy coordinate in an imaginary 2D canvas."]
 #[inline]
 pub fn xy_to_index(x: u32, y: u32, width: u32, height: u32) -> u32 {
     if x > width || y > height {
@@ -20,11 +22,13 @@ pub fn xy_to_index(x: u32, y: u32, width: u32, height: u32) -> u32 {
     y * width + x
 }
 
+#[doc = "Turns a bool into an u32: false -> 0, true -> 1."]
 #[inline]
-pub fn bool_to_int(b: bool) -> i32 {
+pub fn bool_to_int(b: bool) -> u32 {
     if b { 1 } else { 0 }
 }
 
+#[doc = "Clamps an u32 between 0 and 1 (returns 0 if a == 0, returns 1 if a != 0)."]
 #[inline]
 pub fn clamp01(a: u32) -> u32 {
     a.max(0).min(1)
